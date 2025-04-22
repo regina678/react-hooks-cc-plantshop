0e36c1afb3dcbc4cee72e896028dc095b526f031
Regina Wanjiru Kariuki <wanjiruregina@gmail.com>
2025-04-22 13:37:24 +0300

    Add stock toggle functionality to PlantCard component using isSoldOut state

--- src/components/PlantCard.js
+++ src/components/PlantCard.js
@@ -1,16 +1,24 @@
 import React from "react";
 
-function PlantCard() {
+function PlantCard({ plant, onUpdatePlant }) {
+  const { name, image, price, isSoldOut } = plant;
+
+  function handleClick() {
+    const updatedPlant = {
+      ...plant,
+      isSoldOut: !plant.isSoldOut
+    };
+    onUpdatePlant(updatedPlant);
+  }
+
   return (
     <li className="card" data-testid="plant-item">
-      <img src={"https://via.placeholder.com/400"} alt={"plant name"} />
-      <h4>{"plant name"}</h4>
-      <p>Price: {"plant price"}</p>
-      {true ? (
-        <button className="primary">In Stock</button>
-      ) : (
-        <button>Out of Stock</button>
-      )}
+      <img src={image} alt={name} />
+      <h4>{name}</h4>
+      <p>Price: {price}</p>
+      <button className={isSoldOut ? "sold-out" : ""} onClick={handleClick}>
+        {isSoldOut ? "Out of Stock" : "In Stock"}
+      </button>
     </li>
   );
 }
